39906d5d739e2f46f4c9c39ace63eef33df96593
ISIGE, Akira <xia@silvia.com>
2018-05-06 15:01:10 +0900

    package.json と webpack.config.js をちょっと整理

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,60 @@
+const NoConsolePlugin = require('no-console-webpack-plugin');
 
-module.exports = {
-    context: __dirname,
-    entry: {
-        background: "./src/Actions/background.ts",
-        options: "./src/Actions/options.tsx",
-        popup: "./src/Actions/popup.tsx",
-    },
-    output: {
-        path: __dirname + "/dist/js",
-        filename: "[name].js"
-    },
-    node: {
-        global: true
-    },
-    resolve: {
-        extensions: [
-            '.tsx', '.ts', '.jsx', '.js'
-        ]
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: 'ts-loader',
-            },
-            {
-                test: /\.jsx?$/,
-                loader: 'babel-loader',
-                options: {
-                    presets: [
-                        ["env", {
-                            targets: {
-                                browsers: ["firefox >= 57"]
-                            }
-                        }],
-                        "stage-0",
-                        "react"
-                    ]
-                }
-            }
-        ]
+module.exports = (env, argv) => {
+    const mode = argv.mode || process.env.NODE_ENV || 'development';
+
+    const config = {
+        context: __dirname,
+        entry: {
+            background: "./src/Actions/background.ts",
+            options: "./src/Actions/options.tsx",
+            popup: "./src/Actions/popup.tsx",
+        },
+        output: {
+            path: __dirname + "/dist/js",
+            filename: "[name].js",
+        },
+        node: {
+            global: true,
+        },
+        resolve: {
+            extensions: [
+                '.tsx', '.ts', '.jsx', '.js'
+            ]
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: {
+                        loader: 'ts-loader'
+                    },
+                },
+                {
+                    test: /\.jsx?$/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                ["env", {
+                                    targets: {
+                                        browsers: ["firefox >= 57"],
+                                    },
+                                }],
+                                "stage-0",
+                                "react",
+                            ],
+                        },
+                    },
+                },
+            ],
+        },
+        plugins: [],
+    };
+    if (mode === 'production') {
+        config.plugins.push(
+            new NoConsolePlugin()
+        );
     }
+    return config;
 };
