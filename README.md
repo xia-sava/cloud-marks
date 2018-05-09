3acce772793a4ff41c88d5cf50b334684e039ff9
ISIGE, Akira <xia@silvia.com>
2018-05-09 18:42:12 +0900

    AMO登録用パッケージ作成のためにwebpack.config.jsを微修正

--- README.md
+++ README.md
@@ -26,5 +26,5 @@ WebExtension API から充分に取り扱えないため，
 
 # アイコンの出典
 
-<div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
-<div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
+Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,62 @@
-const NoConsolePlugin = require('no-console-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
-module.exports = (env, argv) => {
-    const mode = argv.mode || process.env.NODE_ENV || 'development';
 
-    const config = {
-        context: __dirname,
-        entry: {
-            background: "./src/Actions/background.ts",
-            options: "./src/Actions/options.tsx",
-            popup: "./src/Actions/popup.tsx",
-        },
-        output: {
-            path: __dirname + "/dist/js",
-            filename: "[name].js",
-        },
-        node: {
-            global: true,
-        },
-        resolve: {
-            extensions: [
-                '.tsx', '.ts', '.jsx', '.js'
-            ]
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.tsx?$/,
-                    use: {
-                        loader: 'ts-loader'
+module.exports = {
+    context: __dirname,
+    entry: {
+        background: "./src/Actions/background.ts",
+        options: "./src/Actions/options.tsx",
+        popup: "./src/Actions/popup.tsx",
+    },
+    output: {
+        path: __dirname + "/dist/js",
+        filename: "[name].js",
+    },
+    node: {
+        global: true,
+    },
+    resolve: {
+        extensions: [
+            '.tsx', '.ts', '.jsx', '.js'
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: {
+                    loader: 'ts-loader'
+                },
+            },
+            {
+                test: /\.jsx?$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ["env", {
+                                targets: {
+                                    browsers: ["firefox >= 57"],
+                                },
+                            }],
+                            "stage-0",
+                            "react",
+                        ],
                     },
                 },
-                {
-                    test: /\.jsx?$/,
-                    use: {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: [
-                                ["env", {
-                                    targets: {
-                                        browsers: ["firefox >= 57"],
-                                    },
-                                }],
-                                "stage-0",
-                                "react",
-                            ],
-                        },
-                    },
-                },
-            ],
-        },
-        plugins: [],
-    };
-    if (mode === 'production') {
-        config.plugins.push(
-            new NoConsolePlugin()
-        );
+            },
+        ],
+    },
+    plugins: [],
+    optimization: {
+        minimizer: [
+            new UglifyJSPlugin({
+                uglifyOptions: {
+                    compress: {
+                        drop_console: true
+                    }
+                }
+            })
+        ]
     }
-    return config;
 };
